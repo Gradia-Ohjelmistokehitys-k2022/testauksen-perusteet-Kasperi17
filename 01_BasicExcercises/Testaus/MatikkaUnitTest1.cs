78db7f9061cf93a5fbc72d4b7f3169cc1ab33109
Kasperi Salonen <AD4449@student.jamk.fi>
2023-11-09 09:11:46 +0200

    Tein tehtävään 1 datarow käyttöharjoitusta ja nyt osaan käyttää sitä

--- 01_BasicExcercises/Testaus/MatikkaUnitTest1.cs
+++ 01_BasicExcercises/Testaus/MatikkaUnitTest1.cs
@@ -7,24 +7,25 @@ namespace Testaus
     public class MatikkaUnitTest1
     {
         [TestMethod]
-        public void MiinusLasku_KahdellaKokonaisLuvulla_PalauttaaKokonaisLuvun()
+        [DataRow (1, 1, 0)]
+        [DataRow (1, 1, 1)]
+        [DataRow(10, 5, -10)]
+        public void MiinusLasku_KahdellaKokonaisLuvulla_PalauttaaKokonaisLuvun(int luku1, int luku2, int tulos)
         {
-            //arrange
-            int luku1 = 0;
-            int luku2 = 1;
-            //act
-            int tulos = Laskin.MiinusLasku(luku1, luku2);
-            //assert
-            Assert.AreEqual(-1, tulos);
+            int OdotettuTulos = luku1 - luku2;
+
+            // Assert
+            Assert.AreEqual(tulos, OdotettuTulos);
         }
+
         [TestMethod]
         public void Potenssi_Yhdell‰Kokonaisluvulla_PalauttaaPotenssin()
         {   
             //arrange
-            int luku3 = 101;
+            int luku3 = 5;
             //act
             int tulos1 = Laskin.Potenssi(luku3);
-            Assert.AreEqual(10201, tulos1);
+            Assert.AreEqual(25, tulos1);
         }
 
     }
