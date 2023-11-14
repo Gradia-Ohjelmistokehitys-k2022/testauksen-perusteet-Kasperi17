8db75a2b6f1f08c3ec252a244223d199c8f727ae
Kasperi <gr275829@gradia.fi>
2023-11-14 22:48:00 +0200

    tehtävä 1 tiedosto osio kesken

--- 01_BasicExcercises/Testi/Testaus.cs
+++ 01_BasicExcercises/Testi/Testaus.cs
@@ -23,8 +23,65 @@ namespace FileOperations
             //Assert
             Assert.IsTrue(systemConfig.Count > 0);
 
+
+        }
+        [TestMethod]
+        public void ReadFile_ReturnsListOfSettings_IfFileIstEmpty()
+        {
+            List<string> systemConfig = new List<string>();
+            string path = "C:\\testi";
+            string moi = "\\pekka.txt";
+            // Luo tyhj‰ tiedosto tai k‰yt‰ olemassa olevaa tyhj‰‰ tiedostoa
+
+            // Act
+            systemConfig = File.ReadFile(systemConfig, moi, path);
+
+            // Assert
+            Assert.IsTrue(systemConfig.Count == 0);
+
+
         }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*[TestMethod]
+        public void FileExists_ReturnsTrueIfFileExists()
+        {
+                // Arrange
+                string path = @"F:\Ohjelmointi teht‰v‰t\moi.txt";
+
+                // Act
+                bool fileExists = File.FileOperations.FileExists(path);
+
+                // Assert
+                Assert.IsTrue(fileExists);
+
+
+        }*/
+
+
+
     }
+    
 }
