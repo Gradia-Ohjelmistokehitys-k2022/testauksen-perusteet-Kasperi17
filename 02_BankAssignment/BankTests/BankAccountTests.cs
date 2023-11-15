3635aff9e19544302b14349797622aa0107a279a
Kasperi Salonen <AD4449@student.jamk.fi>
2023-11-15 13:28:00 +0200

    Tehtävä 2, 1_Yksikkotestit on valmiit

---
+++ 02_BankAssignment/BankTests/BankAccountTests.cs
@@ -0,0 +1,65 @@
+using BankAccountNS;
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -1;
+            BankAccount account = new BankAccount("Mr. Milo", beginningBalance);
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {   // Arrange
+            double beginningBalance = 20.55;
+            double debitAmount = 30.99;
+
+            BankAccount account = new BankAccount("Kullervo haavisto", beginningBalance);
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+                
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+    } 
+}
